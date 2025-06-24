3e4a9a573e271f7d709e2fd018b6fea8351ceacc
tomdan-ai <tomudoh258@gmail.com>
2025-06-25 00:00:53 +0100

    feat: remove password hashing and comparison logic from user schema

--- backend/models/User.js
+++ backend/models/User.js
@@ -52,26 +52,5 @@ const userSchema = new mongoose.Schema({
     timestamps: true
 });
 
-// Hashing logic is no longer needed and can be removed.
-/*
-userSchema.pre('save', async function(next) {
-    if (!this.isModified('password')) return next();
-    
-    try {
-        const salt = await bcrypt.genSalt(10);
-        this.password = await bcrypt.hash(this.password, salt);
-        next();
-    } catch (error) {
-        next(error);
-    }
-});
-*/
-
-// Password comparison is no longer needed.
-/*
-userSchema.methods.comparePassword = async function(candidatePassword) {
-    return bcrypt.compare(candidatePassword, this.password);
-};
-*/
 
 module.exports = mongoose.model('User', userSchema);
